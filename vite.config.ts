5001d23386592af00617f4cc840ef273d882b0ac
Isabella D'Otero <isabelladoterob@gmail.com>
2025-11-09 21:34:57 -0500

    FEAT Tailwind y React router dom

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'; // 👈 Importa tailwindcss
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(), // 👈 Añade el plugin
+  ],
+ })
+
