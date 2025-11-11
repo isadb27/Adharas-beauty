69e16fed8ba354b2463e0f8fbeaf85c9f4077a07
Lina Maria Caicedo <linamacaicedo@gmail.com>
2025-11-11 09:13:31 -0500

    footerfinal se me olvido hacer commit antes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
 import Products from "./pages/Products";
+import Footer from "./components/Footer";
 
 export default function App() {
-  return <Products />;
+  return (
+    <div className="min-h-dvh flex flex-col">
+      <main className="flex-1">
+        <Products />
+      </main>
+      <Footer />
+    </div>
+  );
 }
