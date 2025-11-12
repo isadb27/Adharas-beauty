bd06a0a0f7d21b3d3d0620770f24b07b2629b661
Isabella D'Otero <isabelladoterob@gmail.com>
2025-11-11 23:10:39 -0500

    Merge con navbar

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css"; 
+import { BrowserRouter } from "react-router-dom"; // 👈 Agrega esto
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>       {/* 👈 Envuelve tu App dentro del Router */}
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
