e5e1ebc819308c517c6bfd7bcf4ebbad8aeb3794
Lina Maria Caicedo <linamacaicedo@gmail.com>
2025-11-12 02:50:07 -0500

    final banner carousel working

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", // 👈 importante para que lea todos tus componentes
-  ],
-  theme: {
-    extend: {},
-  },
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: { extend: {} },
   plugins: [],
 };
